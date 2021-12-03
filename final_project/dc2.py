3abbe09ba6a3856ff310cf0783a7f2126efca73d
ctcimmarrusti <ctcimmarrusti@aol.com>
2021-12-02 22:54:54 -1000

    Looks like basic framework is created

--- final_project/dc2.py
+++ final_project/dc2.py
@@ -6,8 +6,8 @@ class Route:
 		self.path = ip_path #lsit expected will error in next line if anything else
 		self.cost = len(ip_path) #option to set cost based on latency or throughput later
 
-	def __str__(self):
-		return "dest: " + str(self.dest) + "\npath: " + str(self.path) + "\ncost: " + str(self.cost)
+	def __repr__(self):
+		return "dest: " + str(self.dest) + " path: " + str(self.path) + " cost: " + str(self.cost) + "--" 
 
 class RoutingTable:
 	def __init__(self, own_ip):
@@ -23,16 +23,43 @@ class RoutingTable:
 		r+= "route_table: " + str(self.route_table)
 		return r
 
+#returns 0 if this is the first pathway to the node
+#returns 1 if this is a superior route to the previously tracked route
+#returns -1 if this is an inferior route and ignores the route
+	def addRoute(self, newRoute):
+		new_ip = newRoute.dest
+		if(newRoute.dest in self.route_table.keys()):
+			if n.cost < self.route_table[new_ip].cost: #if new cost is less than current pathway cost, replace pathway
+				self.route_table.update({new_ip: n}) #this allows only one path to any IP
+				return 1
+			else:
+				return -1
+		else:
+			self.route_table[new_ip] = newRoute
+			return 0
+
+#returns 0 if this is the first pathway to the node
+#returns 1 if this is a superior route to the previously tracked route
+#returns -1 if this is an inferior route and ignores the route
 #adds node to allnodes list, adds it to neighbors, creates new route and adds it to routetable
 	def NewNeighbor(self, new_ip):
 		if not (new_ip in self.allNodes):
 			self.allNodes.append(new_ip)
 		if not (new_ip in self.neighbors):
 			self.neighbors.append(new_ip) #track new neighbor
-		n = Route(new_ip, [new_ip]) #create route to new neighbor
-		if(new_ip in self.route_table.keys()):
-			if n.cost < self.route_table[new_ip].cost: #if new cost is less than current pathway cost, replace pathway
-				self.route_table.update({new_ip: n}) #this allows only one path to any IP
+		newR = Route(new_ip, [new_ip]) #create route to new neighbor
+		return self.addRoute(newR)
+
+
+
+#when a route is provided by a neighbor:
+#returns 0 if this is the first pathway to the node
+#returns 1 if this is a superior route to the previously tracked route
+#returns -1 if this is an inferior route and ignores the route
+	def routeFromNeighbor(self, nei_ip, nei_route):
+		nei_route.path.insert(0, nei_ip)
+		nei_route.cost += 1  #LATER: be some sort of reference to cost of the best path to this neighbor
+		return self.addRoute(nei_route)
 
 a = Route(1,[3,2,1])
 print(a)
@@ -41,5 +68,7 @@ n = RoutingTable(1)
 print(n)
 print("-")
 n.NewNeighbor(2)
-n.routeFromNeighbor(2, [3,4])
+r2 = Route(4, [3,4])
+p = n.routeFromNeighbor(2, r2)
+print(p)
 print(n)
